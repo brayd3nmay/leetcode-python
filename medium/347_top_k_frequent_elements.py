683e007331e8493fdabf53a537602371e4c8031e
Brayden May <127856333+brayd3nmay@users.noreply.github.com>
2025-07-02 12:26:35 -0400

    Add solution for Top K Frequent Elements

---
+++ medium/347_top_k_frequent_elements.py
@@ -0,0 +1,25 @@
+# 347. Top K Frequent Elements
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/top-k-frequent-elements
+# Date: 07/01/2025
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqMap = {}
+        freqList = [[] for _ in range(len(nums))]
+
+        for i in nums:
+            freqMap[i] = freqMap.get(i, 0) + 1
+
+
+        # use 0 based indexing for freq
+        for num, freq in freqMap.items():
+            freqList[freq - 1].append(num)
+
+        retArray = []
+        for i in range(len(freqList) - 1, -1, -1):
+            for j in range(len(freqList[i])):
+                retArray.append(freqList[i][j])
+
+            if len(retArray) == k:
+                return retArray
