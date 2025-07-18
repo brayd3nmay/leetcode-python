919278ccef33fa797c73f032dd39a2b203323ef4
Brayden May <127856333+brayd3nmay@users.noreply.github.com>
2025-07-18 14:25:42 -0400

    Add Daily Temperatures solution

---
+++ medium/739_daily_temperatures.py
@@ -0,0 +1,22 @@
+# Problem: 739. Daily Temperatures
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/daily-temperatures/
+# Date: 07/18/2025
+
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(temperatures)
+
+        print(res)
+
+        for i, n in enumerate(temperatures):
+            while stack and n > stack[-1][1]:
+                index, num = stack.pop()
+                res[index] = i - index
+
+            stack.append((i, n))
+
+        return res
