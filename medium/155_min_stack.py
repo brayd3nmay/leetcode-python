ec46543ed5b31b4a8f8a9f757911967df5926c60
Brayden May <127856333+brayd3nmay@users.noreply.github.com>
2025-07-18 10:51:28 -0400

    Update Min Stack solution to optimal

--- medium/155_min_stack.py
+++ medium/155_min_stack.py
@@ -12,25 +12,21 @@ class MinStack:
     def push(self, val: int) -> None:
         self.stack.append(val)
 
-        if len(self.min_stack) == 0:
-            self.min_stack.append(val)
-        elif val < self.min_stack[len(self.min_stack) - 1]:
-            self.min_stack.append(val)
-        else:
-            self.min_stack.append(self.min_stack[len(self.min_stack) - 1])
+        val = min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(val)
 
 
     def pop(self) -> None:
         self.min_stack.pop()
 
-        return self.stack.pop()
+        self.stack.pop()
 
     def top(self) -> int:
-        return self.stack[len(self.stack) - 1]
+        return self.stack[-1]
         
 
     def getMin(self) -> int:
-        return self.min_stack[len(self.min_stack) - 1]
+        return self.min_stack[-1]
     
 
 # Your MinStack object will be instantiated and called as such:
